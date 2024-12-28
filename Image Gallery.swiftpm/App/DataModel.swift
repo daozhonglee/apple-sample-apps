a3cec53d7fea5d774bbe139b9d9d3933bc4574e8
tianfav <tianfav@163.com>
2024-12-28 19:53:55 +0800

    add image gallery

--- Image Gallery.swiftpm/App/DataModel.swift
+++ Image Gallery.swiftpm/App/DataModel.swift
@@ -10,30 +10,37 @@ class DataModel: ObservableObject {
     @Published var items: [Item] = []
     
     init() {
-        // 从文档目录加载已保存的图片
+
+        // 从文档目录加载的图片
+        //FileManager 是一个用于管理文件系统的类，提供了创建、删除、移动、复制文件和目录的功能，以及查询文件属性等操作。它是与文件系统交互的核心工具
         if let documentDirectory = FileManager.default.documentDirectory {
+            debugPrint("333",documentDirectory, FileManager.default.getContentsOfDirectory(documentDirectory))
             let urls = FileManager.default.getContentsOfDirectory(documentDirectory).filter { $0.isImage }
             for url in urls {
+//                debugPrint("1111 Loading image from document directory: \(url)")
                 let item = Item(url: url)
                 items.append(item)
             }
         }
-        
+
         // 从应用程序包中加载默认图片
+        //在 iOS、macOS 等 Apple 平台的应用开发中，主应用程序包（Main Bundle） 是指包含应用程序可执行文件及其所有相关资源（如图片、音频、故事板、本地化文件等）的目录。它是应用程序的核心部分，由 Xcode 在构建应用时自动生成。
+        // forResourcesWithExtension ext: String? 指定要查找的资源文件的扩展名。
+        // subdirectory subpath: String?  指定要查找的资源文件所在的子目录路径。
         if let urls = Bundle.main.urls(forResourcesWithExtension: "jpg", subdirectory: nil) {
+            //  URL 是 Foundation 框架中的 URL 类型，用于表示资源文件的位置路径。URL 是 Swift 中用于处理文件路径、网络地址等资源的通用类型。
             for url in urls {
+//                debugPrint("22222 Loading image from main bundle: \(url)")
                 let item = Item(url: url)
                 items.append(item)
             }
         }
     }
     
-    /// Adds an item to the data collection.
     func addItem(_ item: Item) {
         items.insert(item, at: 0)
     }
     
-    /// Removes an item from the data collection.
     func removeItem(_ item: Item) {
         if let index = items.firstIndex(of: item) {
             items.remove(at: index)
--- Image Gallery.swiftpm/App/GridView.swift
+++ Image Gallery.swiftpm/App/GridView.swift
@@ -4,6 +4,8 @@ See the License.txt file for this sample’s licensing information.
 
 import SwiftUI
 
+//了解如何使用一系列 SwiftUI 内置视图和组合视图组合网格视图。
+
 struct GridView: View {
     // 使用 @EnvironmentObject 接收共享的数据模型
     @EnvironmentObject var dataModel: DataModel
@@ -26,11 +28,14 @@ struct GridView: View {
                 ColumnStepper(title: columnsTitle, range: 1...8, columns: $gridColumns)
                 .padding()
             }
-            // LazyVGrid 用于创建网格布局
+            //ScrollView 允许网格的内容在必要时垂直滚动。
             ScrollView {
+                // LazyVGrid 用于创建网格布局
                 LazyVGrid(columns: gridColumns) {
-                    // ForEach 遍历并显示所有图片项
+                    // ForEach 遍历并显示所有图片项， 由于每个项目实例都是可识别的，因此在创建网格视图时不需要 id 参数
                     ForEach(dataModel.items) { item in
+                        //GeometryReader 是 SwiftUI 中的一个视图容器，用于获取其父视图或自身的大小和位置信息。它允许你根据视图的几何信息（如尺寸、坐标等）动态调整子视图的布局或行为。
+                        // 这里使用主要是为了获取父视图提供的空间大小，以便在 GridItemView 中使用， 否则 GridItemView 的 size 就得硬编码，不能使用不同屏幕尺寸
                         GeometryReader { geo in
                             NavigationLink(destination: DetailView(item: item)) {
                                 GridItemView(size: geo.size.width, item: item)
@@ -38,17 +43,17 @@ struct GridView: View {
                         }
                         .cornerRadius(8.0)
                         .aspectRatio(1, contentMode: .fit)
-                        .overlay(alignment: .topTrailing) {
-                            if isEditing {
+                        .overlay(alignment: .topTrailing) { // // 在右上角添加叠加层
+                            if isEditing { // / 仅在编辑模式显示
                                 Button {
                                     withAnimation {
                                         dataModel.removeItem(item)
                                     }
                                 } label: {
                                     Image(systemName: "xmark.square.fill")
-                                                .font(Font.title)
-                                                .symbolRenderingMode(.palette)
-                                                .foregroundStyle(.white, .red)
+                                                .font(Font.title)  // 设置图标大小
+                                                .symbolRenderingMode(.palette) // 启用多色模式
+                                                .foregroundStyle(.white, .red) // 设置图标颜色：白色X，红色背景
                                 }
                                 .offset(x: 7, y: -7)
                             }
@@ -75,7 +80,7 @@ struct GridView: View {
                 } label: {
                     Image(systemName: "plus")
                 }
-                .disabled(isEditing)
+                .disabled(isEditing) // // 编辑模式下禁用编辑
             }
         }
     }
